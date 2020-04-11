da308e3092f134a8333e8fe059ec229c3e5de2ba
larissae24 <larissae24@googlemail.com>
2020-04-11 22:15:22 +0200

    footer repsonsive

--- blog/src/components/footer-larissa/footer-larissa.tsx
+++ blog/src/components/footer-larissa/footer-larissa.tsx
@@ -6,22 +6,48 @@ import { Component, Prop, h } from '@stencil/core';
   shadow: true
 })
 export class FooterLarissa {
-@Prop() erster!: string;
-@Prop() zweiter: string;
-@Prop() dritter: string;
-@Prop() ersterlink!: string;
-@Prop() zweiterlink: string;
-@Prop() dritterlink: string;
+  @Prop() erster!: string;
+  @Prop() zweiter: string;
+  @Prop() dritter: string;
+  @Prop() ersterlink!: string;
+  @Prop() zweiterlink: string;
+  @Prop() dritterlink: string;
+
+  public icons: Array<any> = [
+    {      name: 'twitter',
+    url: 'https://twitter.com/explore',
+    imageUrl: '/assets/twitter.png'
+    },
+    {
+      name: 'insta',
+      url: 'https://www.instagram.com/?hl=de',
+      imageUrl: '/assets/insta.png'
+    },
+    {
+      name: 'facebook',
+      url: 'https://www.facebook.com',
+      imageUrl: '/assets/facebook.png'
+    },
+  ]
 
   render() {
     return (
         <div class="footer-larissa">
-        <hr id="trennlinie"></hr>
+        <div id="trennlinie"></div>
+        
+        <div class="wrapper">
+        <div class="icons">{this.icons.map(entry =>
+          <a href={entry.url}> <img class="img" src={entry.imageUrl} /></a>)}
+        </div>
+
+        <div class="links">
         <ul id="footer-liste">
-        <li class="footer-link"><a href={this.ersterlink}>{this.erster}</a></li>
-        <li class="footer-link"><a href={this.zweiterlink}>{this.zweiter}</a></li>
-        <li class="footer-link"><a href={this.dritterlink}>{this.dritter}</a></li>
+        <li class="footer-link"><a class="text-link" href={this.ersterlink}>{this.erster}</a></li>
+        <li class="footer-link"><a class="text-link" href={this.zweiterlink}>{this.zweiter}</a></li>
+        <li class="footer-link"><a class="text-link" href={this.dritterlink}>{this.dritter}</a></li>
         </ul>
+        </div>
+        </div>
         </div>
     );
   }
