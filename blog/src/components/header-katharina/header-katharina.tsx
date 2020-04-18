3cdc32989898b4e421e0a3ed4fb3e2d4057f4a31
katathering <57351169+katathering@users.noreply.github.com>
2020-04-18 20:20:06 +0200

    burger menu dropdown added, responsive design added

--- blog/src/components/header-katharina/header-katharina.tsx
+++ blog/src/components/header-katharina/header-katharina.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, getAssetPath } from '@stencil/core';
+import { Component, h, Prop, State} from '@stencil/core';
 
 @Component({
     tag: 'header-katharina',
@@ -15,13 +15,39 @@ export class HeaderKatharina {
     @Prop() logo: 'default_logo.png';
     @Prop() burger: 'Hamburger_icon.png';
 
+    @State() showMenu = false;
+
+    showMenuHandler(){
+        if(!this.showMenu){
+            this.showMenu = true;
+        }else{
+            this.showMenu = false;
+        }
+    }
+
 
     render (){
+        let options = null;
+        if(this.showMenu){
+            options = 
+                <div class='menu'>
+                <div class="burger-links">
+                        <a href="#">{this.textone}</a><br></br>
+                        <a href="#">{this.texttwo}</a><br></br>
+                        <a href="#">{this.textthree}</a><br></br>
+                        <a href="#">{this.textfour}</a><br></br>
+                        <a href="#">{this.textfive}</a><br></br>
+                </div>
+                </div>
+        }else{
+            options = null;
+        }
+
         return (
             <div class="wrapper">
                 <div class="header">
                     <ul class="logo">
-                        <li><a href="#" class="logo"><img src={getAssetPath(`../assets/${this.logo}`)} alt="Logo" /></a></li>
+                        <li><a href="#" class="logo"><img class='logo-image' src='/assets/twitter.png' alt="Logo" /></a></li>
                     </ul>
                     <ul class="links">
                         <li><a href="#">{this.textone}</a></li>
@@ -31,21 +57,13 @@ export class HeaderKatharina {
                         <li><a href="#">{this.textfive}</a></li>
                     </ul>
 
-
-                    <div id="burger-links">
-                        <a href="#">{this.textone}</a>
-                        <a href="#">{this.texttwo}</a>
-                        <a href="#">{this.textthree}</a>
-                        <a href="#">{this.textfour}</a>
-                        <a href="#">{this.textfive}</a>
-                    </div>
-
-                    <div class='burger-wrapper'>
-                        <img src={getAssetPath(`../assets/${this.burger}`)} alt="Menu" />
-                    </div>
-
+                    <ul class='burger-wrapper'>
+                        <li><button class='burgerBtn' onClick={this.showMenuHandler.bind(this)}><img class='burger-icon' src='/assets/facebook.png' alt="Menu" /></button></li>
+                    </ul>
+                    
                     <div class="border"></div>
                 </div>
+                {options}
             </div>
         )
 
